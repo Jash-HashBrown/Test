487e9aa0d59728e29135835dce6a313400ff4b03
Jash <jash.acharya.24@gmail.com>
2025-06-17 03:16:38 +0530

    Misc

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(bodyParser.json());
-
-app.get("/getuser", (req, res) => {
-  res.send({ status: 1, message: "We are up! Baby" });
+app.get("/", (req, res) => {
+  res.send("Hello world!");
 });
 
 app.listen(PORT, () => {
